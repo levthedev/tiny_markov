26a5e8c9d861aac30bc87692d97ef84b8da93b46
Lev Kravinsky <kravinskylev@gmail.com>
2016-03-21 17:24:50 -0400

    8 LoC markov chain (2 minimized, 129 bytes)

--- 2chainz.rb
+++ 2chainz.rb
@@ -1,7 +1,7 @@
 stems = ARGF.read.split.each_cons(2).group_by {|pair| pair[0]}
+puts stems
 root, chain = stems.first.first, ""
-stems.size.times do
-  break if !stems[root]
+while stems[root] && chain.length < stems.length
   root = stems[root].sample.last
   chain += "#{root} "
 end
