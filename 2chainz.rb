f2c10098f71eac68d73eada40566e17eb1f460b1
Lev Kravinsky <kravinskylev@gmail.com>
2016-03-21 17:32:54 -0400

    length constraint

--- 2chainz.rb
+++ 2chainz.rb
@@ -1,7 +1,7 @@
 stems = ARGF.read.split.each_cons(2).group_by {|pair| pair[0]}
-puts stems
 root, chain = stems.first.first, ""
-while stems[root] && chain.length < stems.length
+stems.size.times do
+  break if !stems[root]
   root = stems[root].sample.last
   chain += "#{root} "
 end
